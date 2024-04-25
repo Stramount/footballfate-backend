18369a1249d1ca9f3aa72c86cd5ca28d7004b691
Artec <its.javierjimenez@gmail.com>
2024-04-25 15:29:23 -0300

    Update path.routes.js
    Se añadió rutas ejemplo para Jugadores y API,  se deben rellenar y/o modificar
    
    El .use para /api/jugadores es un middleware para saber si hay un parametro o no, podria ser usado para cargar  comprobar el cache y tener preparado los datos para que el siguiente handler los use
    
    hay un forbidden, que intenté obtener el archivo HTML para mostrarlo pero me lo prohibe

--- src/routes/path.routes.js
+++ src/routes/path.routes.js
@@ -1,9 +1,32 @@
 import { Router } from "express";
 
-
 export const router = Router()
-const defaultPath = "/api/"
+
+const defaultPath = "/"
+const apiPath = "/api/"
+
+// mensaje para la direccion localhost/8008/
+router.get(defaultPath , (req , res) => res.send("Root"))
+
+// hacer alguna confirmación para proteger la api
+router.use(apiPath , (req , res, next) => next())
+
+router.get(apiPath , (req , res) => res.send("hi")) // bienvenida a la API
+
+// aqui defino un middleware para saber si hay un parametro despues de /jugadores
+router.use(apiPath + "jugadores" , (req , res , next) => {
+    //podriamos aprovechar y tener los datos preparados antes de pasar
+    //al get
 
 
-router.get(defaultPath + "jugadores/:param" , (req , res) => res.status(200).send(req.params.param))
-router.get(defaultPath , (req , res) => res.send("hi"))
+    // expresion regular, para enviarlo al get con PARAM o mantenerlo acá
+   if(/\/.([^/]+)/.test(req.url) && req.method == "GET"){
+       
+       return next() // continuamos al router get
+   }
+
+   return res.send("Aqui los jugadores")
+})
+
+router.get(apiPath + "jugadores/:param",
+          (req , res) => res.status(200).header("Content-Type" , "text/html").sendFile("../../DATA/htmlprueba.html" , {root: __dirname})) //solucionar este forbidden
