1710e0960dca94b16ad71cb0fd520ac12f56a938
Artec <106358743+ArtecOne@users.noreply.github.com>
2024-04-25 22:28:30 -0300

    Update path.routes.js
    se definió las rutas a usar en la Iteración 1

--- src/routes/path.routes.js
+++ src/routes/path.routes.js
@@ -2,31 +2,14 @@ import { Router } from "express";
 
 export const router = Router()
 
-const defaultPath = "/"
 const apiPath = "/api/"
 
-// mensaje para la direccion localhost/8008/
-router.get(defaultPath , (req , res) => res.send("Root"))
-
 // hacer alguna confirmación para proteger la api
-router.use(apiPath , (req , res, next) => next())
+router.use(apiPath , (req , res, next) => {console.log("middleware") ; next()})
 
 router.get(apiPath , (req , res) => res.send("hi")) // bienvenida a la API
 
-// aqui defino un middleware para saber si hay un parametro despues de /jugadores
-router.use(apiPath + "jugadores" , (req , res , next) => {
-    //podriamos aprovechar y tener los datos preparados antes de pasar
-    //al get
+router.all(apiPath + "cuenta" , (req , res) => res.send("todos los metodos para cuenta, post, get, put, delete, etc"))
 
-
-    // expresion regular, para enviarlo al get con PARAM o mantenerlo acá
-   if(/\/.([^/]+)/.test(req.url) && req.method == "GET"){
-       
-       return next() // continuamos al router get
-   }
-
-   return res.send("Aqui los jugadores")
-})
-
-router.get(apiPath + "jugadores/:param",
-          (req , res) => res.status(200).header("Content-Type" , "text/html").sendFile("../../DATA/htmlprueba.html" , {root: __dirname})) //solucionar este forbidden
+router.get(apiPath + "equipo/:user" , (req , res) => res.send("obtener a " + req.params.user))
+router.put(apiPath + "equipo/:user" , (req , res) => res.send("modificar a " + req.params.user))
