db7254696fa18c09bcacb452af1b66d63aaa6f6a
sebas <moschen.sebastian.et21.22@gmail.com>
2024-05-04 18:42:36 -0300

    Modelo nuevo de BD

--- SQL/Code.sql
+++ SQL/Code.sql
@@ -1,23 +1,81 @@
-create table Equipo(
-	ID_Equipo INT not null auto_increment primary key,
-    NombreEquipo varchar(75) not null,
-	Puntuacion int(4) not null
-);
-
 Create table Usuario(
-    ID_Usuario int NOT NULL auto_increment primary key,
+    ID int NOT NULL auto_increment primary key,
     Nickname char(35) not null,
-    ID_Equipo int not null,
     Contraseña char(32) not null,
     Mail char(54) not null,
-    Presupuesto float(3) not null,
-    foreign key(ID_Equipo) references Equipo(ID_Equipo)
+    Presupuesto float(3) not null
 );
 
+CREATE table Fecha(
+    ID int not null auto_increment primary key,
+    fecha datetime not null
+);
 
+CREATE table Jugador(
+    ID INT NOT NULL auto_increment primary key,
+    nombre char(15) not null,
+    apellido char(20) not null,
+    categoria char(3) NOT NULL,
+    precio int not null
+);
 
-CREATE index Index1 
-on Usuario (ID_Usuario, Nickname, ID_Equipo, Contraseña, Mail, Presupuesto);
+CREATE table Alineacion(
+    ID INT NOT NULL auto_increment primary key,
+    posgk int not null,
+    pos1 int not null,
+    pos2 int not null,
+    pos3 int not null,
+    pos4 int not null,
+    pos5 int not null,
+    pos6 int not null
+);
 
-Create index Index2
-on Equipo (ID_Equipo, NombreEquipo, Puntuacion);
+CREATE table Banca(
+    ID INT NOT NULL auto_increment primary key,
+    pos1 int not null,
+    pos2 int not null
+);
+
+create table Equipo(
+	ID INT not null auto_increment primary key,
+    NombreEquipo varchar(75) not null,
+	Puntuacion int(4) not null,
+    ID_Usuario int not null,
+    ID_Banca int not null,
+    ID_Alineacion int not null,
+    foreign key(ID_Usuario) references Usuario(ID),
+    foreign key(ID_Banca) references Banca(ID),
+    foreign key(ID_Alineacion) references Alineacion(ID)
+);
+
+CREATE table Estadistica(
+    ID INT not NULL auto_increment primary key,
+    ID_Fecha INT not null,
+    ID_Jugador int not null,
+    foreign key(ID_Fecha) references Fecha(ID),
+    foreign key(ID_Jugador) references Jugador(ID)
+);
+
+CREATE table Equipo_Fecha(
+    ID_Equipo int not null,
+    ID_Fecha int not null,
+    primary key(ID_Equipo, ID_Fecha),
+    foreign key(ID_Equipo) references Equipo(ID),
+    foreign key(ID_Fecha) references Fecha(ID)
+);
+
+create table Jugador_Alineacion(
+    ID_Alineacion int not null,
+    ID_Jugador int not NULL,
+    primary key(ID_Alineacion, ID_Jugador),
+    foreign key(ID_Alineacion) references Alineacion(ID),
+    foreign key(ID_Jugador) references Jugador(ID)
+);
+
+create table Jugador_Banca(
+    ID_Banca int not null,
+    ID_Jugador int not NULL,
+    primary key(ID_Banca, ID_Jugador),
+    foreign key(ID_Banca) references Alineacion(ID),
+    foreign key(ID_Jugador) references Jugador(ID)
+);
