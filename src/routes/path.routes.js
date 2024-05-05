c65f01ff047b8e452b32135df5797ab3bb1a3c0f
lautaro <chaparro.lautaro.et21.21@gmail.com>
2024-05-04 21:52:04 -0300

    add validations auth routes

--- src/routes/path.routes.js
+++ src/routes/path.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { AdminRouter } from "./admin.routes.js";
 import mainController from "../controllers/main.controller.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
 
 export const APIrouter = Router()
-
 
 // hacer alguna confirmación para proteger la api
 APIrouter.use("/admin" , AdminRouter)
@@ -15,13 +15,18 @@ APIrouter.use("/" , (req , res, next) => {
 
 APIrouter.get("/", (req , res) => res.send("hi")) // bienvenida a la API
 
-APIrouter.post("/auth/login" , mainController.database.entities.account.handleAccount) // router para login (POST)
-APIrouter.post("/auth/register" , mainController.database.entities.account.handleAccount) // router para registro (POST)
+APIrouter.post("/auth/login", mainController.validator.validateSchema(loginSchema), mainController.database.entities.account.handleAccount) // router para login (POST)
+APIrouter.post("/auth/register", mainController.validator.validateSchema(registerSchema), mainController.database.entities.account.handleAccount) // router para registro (POST)
 
-APIrouter.all(["/cuenta" , "/cuenta/:email"] , mainController.database.entities.account.handleAccount) // router para la cuenta (GET , PATCH)
+APIrouter.all(["/cuenta" , "/cuenta/:id"], mainController.validator.validationToken, mainController.database.entities.account.handleAccount) // router para la cuenta (GET , PATCH)
 
-APIrouter.all(["/usuario", "/usuario/:user"], mainController.database.entities.user.handleUser) // router para el usuario (GET , PATCH)
+APIrouter.all(["/usuario", "/usuario/:user"], mainController.validator.validationToken, mainController.database.entities.user.handleUser) // router para el usuario (GET , PATCH)
 
-APIrouter.all(["/equipo", "/equipo/:nombreEquipo"], mainController.database.entities.team.handleTeam) // leer un equipo o editarlo
+APIrouter.patch(["/equipo", "/equipo/:nombreEquipo"], mainController.validator.validationToken, mainController.database.entities.team.handleTeam) // leer un equipo o editarlo
 
-APIrouter.all(["/jugador", "/jugador/:name"] , mainController.database.entities.player.handlePlayer) // leer un jugador o editarlo
+APIrouter.get(["/equipo", "/equipo/:nombreEquipo"], mainController.database.entities.team.handleTeam) // leer un equipo o editarlo
+
+APIrouter.patch(["/jugador", "/jugador/:name"], mainController.validator.validationToken, mainController.database.entities.player.handlePlayer) // leer un jugador o editarlo
+
+APIrouter.get(["/jugador", "/jugador/:name"], mainController.database.entities.player.handlePlayer) // leer un jugador o editarlo
+
