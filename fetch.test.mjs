c4a7ce3b6cb3c81f70fdba5a8436e819f40c5af9
Artec <jimenez.javier.et21.21@gmail.com>
2024-05-30 09:45:09 -0300

    CORS active

--- fetch.test.mjs
+++ fetch.test.mjs
@@ -1,10 +1,6 @@
 import test from "node:test"
 import assert from "node:assert/strict";
 
-test("prueba" , () => {
+test("prueba API" , () => {
     assert.strictEqual(1 , 1)
-})
-
-test("prueba2" , () => {
-    assert.strictEqual(2 , 2)
 })
